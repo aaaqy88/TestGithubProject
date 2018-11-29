626b6bae95985dd88c955ad6861c4ec761d1c756
aaaqy88 <970978847@qq.com>
2018-11-29 09:33:00 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:zz
+ * createTime:2018-11-29 09:07
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试guthub提交111111111");
+    }
+}
